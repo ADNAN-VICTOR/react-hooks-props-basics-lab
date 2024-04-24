a870671ff1a236170041ba745920a6ac38eb65c2
adnan <adnan.victor@student.moringaschool.com>
2024-04-24 18:58:37 +0300

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,30 @@
 import React from "react";
 
-function About() {
+function Links(props){
+  return(
+    <a href={props.link}>{props.text}</a>
+  )
+}
+
+
+
+function Bio(props){
+  return (
+    <p>{props.Bio}</p>
+  )
+}
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <Bio Bio={props.bio}/>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links link="https://github.com/liza" text="https://github.com/liza"/>
+      <Links link="https://www.linkedin.com/in/liza/" text="https://www.linkedin.com/in/liza/"/>
     </div>
   );
 }
+
 
 export default About;
